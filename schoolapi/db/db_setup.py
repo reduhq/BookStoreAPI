81163b4931e33ca56cdbb3ebe697df60c48c921b
ReyEduardo836 <reyeduardohalsallquintero8@gmail.com>
2022-12-22 13:09:39 -0600

    Creating an Async engine to make our api asynchronous

--- schoolapi/db/db_setup.py
+++ schoolapi/db/db_setup.py
@@ -1,12 +1,14 @@
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
 
 from pydantic import PostgresDsn
 
 SQLALCHEMY_DATABASE_URL = os.environ.get('SQLALCHEMY_DATABASE_URL')
+SQLALCHEMY_DATABASE_URL_ASYNC = os.environ.get('SQLALCHEMY_DATABASE_URL_ASYNC')
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={}, future=True
@@ -14,9 +16,6 @@ engine = create_engine(
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine, future=True
 )
-
-Base = declarative_base()
-
 #DB Itilities
 def get_db():
     db = SessionLocal()
@@ -24,3 +23,18 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# ASYNC ENGINE
+async_engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL_ASYNC
+)
+AsyncSessionLocal = sessionmaker(
+    async_engine, class_=AsyncSession, expire_on_commit=False
+)
+#DB Itilities
+async def async_get_db():
+    async with AsyncSessionLocal() as db:
+        yield db
+        await db.commit()
+
+Base = declarative_base()
